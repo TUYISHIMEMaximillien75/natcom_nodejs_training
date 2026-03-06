8aa1ec6e14d962126ff8f64cef61b7fe880bf265
MATY Codes <tuyishimemaximillien@gamil.com>
2026-03-06 15:57:21 +0200

    second commit

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router";
+import { Routes, Route, Link} from "react-router";
 import Table from "./components/Table";
 import Add from "./components/Add";
 function App() {
@@ -6,14 +6,16 @@ function App() {
   return (
     <>
     {/* <Table></Table> */}
+      <Link to="add">Add</Link>
+
       <h1>User for Natcom website</h1>
       <p>Welcome to the Natcom website! We are dedicated to providing you with the best communication services. Whether you're looking for mobile plans, internet packages, or customer support, we've got you covered. Explore our offerings and find the perfect solution for your communication needs.</p>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
         <Routes>
-          <Route path="/table" element={<Table/>}></Route>
+          <Route path="/" element={<Table/>}></Route>
           <Route path="/add" element={<Add/>}></Route>
         </Routes>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </>
   )
 }
--- front_end/src/components/Add.jsx
+++ front_end/src/components/Add.jsx
@@ -1,33 +1,49 @@
 import { useState } from "react"
 import axios from "axios";
-function Add(){
+function Add() {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState(null)
-    const handleForm = ()=>{
-        axios.post('http://localhost:8000/adduser', {
-            name: name,
-            password: password
-        }).then((response)=>{
-            console.log(response.data)
-        })
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const handleForm = (event) => {
+        event.preventDefault();
+
+        if (name == "" || password == "") {
+            setError("Fill all fields")
+        } else {
+        setLoading(true)
+
+            axios.post('http://localhost:8000/adduser', {
+                name: name,
+                password: password
+            }).then((response) => {
+                setMessage(response.data.message)
+                setError(null)
+                setLoading(false)
+            })
+        }
+
+
     }
-    return(
+    return (
         <>
-        <form action="" onSubmit={handleForm}>
-            <div className="form-group">
-                <label htmlFor="">Name</label>
-                <input type="text" onChange={(e)=>setName(e.target.value)}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="">Password</label>
-                <input type="password" onChange={(e)=>setPassword(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <button>Save</button>
-            </div>
-            {message}
-        </form>
+            <form onSubmit={handleForm}>
+                <div className="form-group">
+                    <label htmlFor="">Name</label>
+                    <input type="text" onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Password</label>
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <button onClick={handleForm}>{loading ? "Saving..." : "Save"}</button>
+                </div>
+                {error}
+                {message}
+            </form>
         </>
     )
 }
